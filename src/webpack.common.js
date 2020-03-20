9af2cbccbf590ff583dd59a2b3d47c4e5240f746
Andrew Leader <aleader@microsoft.com>
2020-03-19 17:19:34 -0700

    Use production scripts

--- src/webpack.common.js
+++ src/webpack.common.js
@@ -1,7 +1,3 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin')
-const CopyPlugin = require("copy-webpack-plugin");
-
 module.exports = {
   entry: "./src/index.tsx",
   output: {
@@ -53,51 +49,5 @@ module.exports = {
             ]
           }
       ]
-  },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  // externals: {
-  //     "react": "React",
-  //     "react-dom": "ReactDOM"
-  // },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new HtmlWebpackExternalsPlugin({
-      externals: [
-        {
-          module: 'google-roboto',
-          entry: {
-            path: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
-            type: 'css',
-          }
-        },
-        {
-          module: 'google-material-icons',
-          entry: {
-            path: 'https://fonts.googleapis.com/icon?family=Material+Icons',
-            type: 'css',
-          }
-        },
-        {
-          module: "react",
-          entry: "umd/react.development.js",
-          global: "React"
-        },
-        {
-          module: "react-dom",
-          entry: "umd/react-dom.development.js",
-          global: "ReactDOM"
-        }
-      ]
-    }),
-    new CopyPlugin([
-      { from: 'public' }
-    ])
-  ]
+  }
 };
